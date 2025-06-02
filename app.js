9a3fcccd83c176fc897fc00b6cced92f1c5b08ad
ismaelortiz87 <iortizmontero@gmail.com>
2025-06-01 22:43:26 -0400

    fix port issue

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import dotenv  from "dotenv"
+import dotenv from "dotenv";
 import express from 'express';
 import parser from 'body-parser';
 import apiRouter from './src/routes/api.js';
@@ -19,10 +19,11 @@ app.use(parser.json());
 app.use('/', apiRouter);
 
 /* Initialize the API server */
-const port = process.env.API_PORT || 3001;
+const port = process.env.port || process.env.API_port || 3001;
+const host = process.env.host || '0.0.0.0';
 
-app.listen( port, () => {
-  console.log(`Server running on: ${port}.`)
+app.listen(port, host, () => {
+  console.log(`Server running on: ${port}.`);
 });
 
 export default app;
